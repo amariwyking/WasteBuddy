27171a56ca635de3cdcfed24e5e1bf1cf9a43f49
Amari Garrett <amarig@fb.com>
2020-07-31 12:41:04 -0400

    - User can see the projects an item is used for

--- app/src/main/java/com/example/wastebuddy/fragments/ItemDetailsFragment.java
+++ app/src/main/java/com/example/wastebuddy/fragments/ItemDetailsFragment.java
@@ -2,6 +2,7 @@ package com.example.wastebuddy.fragments;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,12 +14,15 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.wastebuddy.ProjectsAdapter;
 import com.example.wastebuddy.R;
 import com.example.wastebuddy.databinding.FragmentItemDetailsBinding;
 import com.example.wastebuddy.models.Item;
+import com.example.wastebuddy.models.Project;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseQuery;
@@ -26,6 +30,8 @@ import com.parse.ParseQuery;
 import org.apache.commons.text.WordUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class ItemDetailsFragment extends Fragment {
@@ -43,10 +49,14 @@ public class ItemDetailsFragment extends Fragment {
 
     TextView mNameTextView;
     TextView mDescriptionTextView;
+    TextView mProjectsTextView;
     TextView mItemNotFoundTextView;
     ImageView mDisposalImageView;
     ImageView mItemImageView;
     RecyclerView mRecyclerView;
+
+    List<Project> mProjects;
+    ProjectsAdapter mProjectsAdapter;
 
     public ItemDetailsFragment() {
         // Required empty public constructor
@@ -68,6 +78,8 @@ public class ItemDetailsFragment extends Fragment {
 
         mBinding = FragmentItemDetailsBinding.inflate(inflater, container, false);
         mContext = getContext();
+        mProjects = new ArrayList<>();
+        mProjectsAdapter = new ProjectsAdapter(getContext(), mProjects);
         bindViews();
         return mBinding.getRoot();
     }
@@ -80,14 +92,26 @@ public class ItemDetailsFragment extends Fragment {
 
         try {
             getItem();
-//            Log.i(TAG, "Found item with the Object ID: " + mItemId);
         } catch (ParseException e) {
             e.printStackTrace();
             Log.e(TAG, "Could not find item with the Object ID: " + mItemId, e);
         }
     }
 
+    private void bindViews() {
+        mNameTextView = mBinding.itemNameTextView;
+        mDescriptionTextView = mBinding.descriptionTextView;
+        mProjectsTextView = mBinding.projectsTextView;
+        mItemNotFoundTextView = mBinding.itemNotFoundTextView;
+        mDisposalImageView = mBinding.disposalImageView;
+        mItemImageView = mBinding.itemImageView;
+        mRecyclerView = mBinding.projectsRecyclerView;
+        configureRecyclerView(mRecyclerView, mProjectsAdapter);
+    }
+
     private void bindData() {
+        queryProjects();
+
         // Bind the item data to the view elements
         mNameTextView.setText(WordUtils.capitalizeFully(mItem.getName()));
         mDescriptionTextView.setText(mItem.getDescription());
@@ -100,36 +124,26 @@ public class ItemDetailsFragment extends Fragment {
         }
     }
 
-    private void setDisposal(Item item, ImageView disposalImageView) {
-        switch (item.getDisposal().toLowerCase()) {
-            case "recycle":
-                disposalImageView.setBackgroundTintList(getResources().getColorStateList(R.color.colorRecycle));
-                disposalImageView.setImageResource(R.drawable.ic_recycle_24);
-                break;
-            case "compost":
-                disposalImageView.setBackgroundTintList(getResources().getColorStateList(R.color.colorCompost));
-                disposalImageView.setImageResource(R.drawable.ic_round_compost_24);
-                break;
-            case "landfill":
-                disposalImageView.setBackgroundTintList(getResources().getColorStateList(R.color.colorLandfill));
-                disposalImageView.setImageResource(R.drawable.ic_round_trash_24);
-                break;
-            case "special":
-                disposalImageView.setBackgroundTintList(getResources().getColorStateList(R.color.colorSpecial));
-                disposalImageView.setImageResource(R.drawable.ic_round_warning_24);
-                disposalImageView.setColorFilter(Color.BLACK);
-                break;
-            default:
-                break;
-        }
-    }
+    private void configureRecyclerView(RecyclerView recyclerView, ProjectsAdapter adapter) {
+        recyclerView.setAdapter(adapter);
+//        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(),
+                RecyclerView.HORIZONTAL, false));
 
-    private void bindViews() {
-        mNameTextView = mBinding.itemNameTextView;
-        mDescriptionTextView = mBinding.descriptionTextView;
-        mItemNotFoundTextView = mBinding.itemNotFoundTextView;
-        mDisposalImageView = mBinding.disposalImageView;
-        mItemImageView = mBinding.itemImageView;
+        final int spacing =
+                getResources().getDimensionPixelSize(R.dimen.margin_padding_size_medium) / 2;
+
+        recyclerView.setPadding(spacing, spacing, spacing, spacing);
+        recyclerView.setClipToPadding(false);
+        recyclerView.setClipChildren(false);
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(@NotNull Rect outRect, @NotNull View view,
+                                       @NotNull RecyclerView parent,
+                                       @NotNull RecyclerView.State state) {
+                outRect.set(spacing, spacing, spacing, spacing);
+            }
+        });
     }
 
     private void getItem() throws ParseException {
@@ -161,5 +175,47 @@ public class ItemDetailsFragment extends Fragment {
         mNameTextView.setVisibility(View.GONE);
         mDescriptionTextView.setVisibility(View.GONE);
         mItemNotFoundTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void setDisposal(Item item, ImageView disposalImageView) {
+        switch (item.getDisposal().toLowerCase()) {
+            case "recycle":
+                disposalImageView.setBackgroundTintList(getResources().getColorStateList(R.color.colorRecycle));
+                disposalImageView.setImageResource(R.drawable.ic_recycle_24);
+                break;
+            case "compost":
+                disposalImageView.setBackgroundTintList(getResources().getColorStateList(R.color.colorCompost));
+                disposalImageView.setImageResource(R.drawable.ic_round_compost_24);
+                break;
+            case "landfill":
+                disposalImageView.setBackgroundTintList(getResources().getColorStateList(R.color.colorLandfill));
+                disposalImageView.setImageResource(R.drawable.ic_round_trash_24);
+                break;
+            case "special":
+                disposalImageView.setBackgroundTintList(getResources().getColorStateList(R.color.colorSpecial));
+                disposalImageView.setImageResource(R.drawable.ic_round_warning_24);
+                disposalImageView.setColorFilter(Color.BLACK);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void queryProjects() {
+        // Specify which class to query
+        ParseQuery<Project> query = ParseQuery.getQuery(Project.class);
+        query.whereEqualTo(Project.KEY_ITEMS, mItemId);
+        query.findInBackground((projects, e) -> {
+            if (e != null) {
+                Log.e(TAG, "Problem  with getting projects", e);
+                return;
+            }
+
+            if (!projects.isEmpty()) {
+                mProjectsTextView.setVisibility(View.VISIBLE);
+                mProjects.addAll(projects);
+                mProjectsAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
